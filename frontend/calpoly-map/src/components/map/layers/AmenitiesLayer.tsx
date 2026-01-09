96b7a74972dd924a9f08eec59a8de9736d8b9e90
Scout Knight-Pheng <sknightp@calpoly.edu>
2026-01-09 13:54:44 -0800

    feat: added an amenities layer with circleLayer and set up amenity filter by category

---
+++ frontend/calpoly-map/src/components/map/layers/AmenitiesLayer.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import type { FeatureCollection, Point } from "geojson";
+import {
+  ShapeSource,
+  CircleLayer,
+} from "@maplibre/maplibre-react-native";
+import features from "../../../../geojson_files/amenities.json";
+
+const amenityTypes = ["bathroom", "printer", "water_fountain"];
+
+type AddGeoJSONLayerArgs = {
+  sourceId: string;
+  layerId: string;
+  data: FeatureCollection<Point>;
+  amenityTypes: string[];
+  circleStyle: {
+    circleRadius: number;
+    circleColor: string;
+    circleStrokeWidth: number;
+    circleStrokeColor: string;
+  };
+};
+
+function addGeoJSONLayer({
+  sourceId,
+  layerId,
+  data,
+  amenityTypes,
+  circleStyle,
+}: AddGeoJSONLayerArgs) {
+  return (
+    <ShapeSource id={sourceId} shape={data}>
+      <CircleLayer
+        id={layerId}
+        filter={[
+          "in",
+          ["get", "category"],
+          ["literal", amenityTypes],
+        ]}
+        style={circleStyle}
+      />
+    </ShapeSource>
+  );
+}
+
+export function AmenitiesLayer() {
+  return (
+    <>
+      {addGeoJSONLayer({
+        sourceId: "amenities-source",
+        layerId: "amenities-layer",
+        data: features as FeatureCollection<Point>,
+        amenityTypes: amenityTypes,
+        circleStyle: {
+          circleRadius: 8,
+          circleColor: "#fff422ff",
+          circleStrokeWidth: 2,
+          circleStrokeColor: "#ffffffff",
+        },
+      })}
+    </>
+  );
+}
