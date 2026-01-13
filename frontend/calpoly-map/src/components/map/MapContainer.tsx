7d26e0ff833aa8086773942505bdcaa8c92e9f23
Rodney <rodney.fujiyama@gmail.com>
2026-01-12 21:08:38 -0800

    feat: constrain map to campus bounds

--- frontend/calpoly-map/src/components/map/MapContainer.tsx
+++ frontend/calpoly-map/src/components/map/MapContainer.tsx
@@ -5,16 +5,30 @@ import { StyleSheet } from 'react-native';
 // Disable telemetry
 setAccessToken(null);
 
+// where map starts (center)
+const CENTER: [number, number] = [-120.6596, 35.3010];
+
+// bounds for map
+const BOUNDS = {
+  sw: [-120.6756, 35.2949] as [number, number],
+  ne: [-120.6438, 35.3162] as [number, number],
+};
+
 export function MapContainer({children}: {children?: React.ReactNode}) {
     return (
         <MapView
             style={styles.map}
             mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
             logoEnabled={false}
+            scrollEnabled
+            zoomEnabled
         >
             <Camera
-                centerCoordinate={[-120.6596, 35.3050]}
-                zoomLevel={15}
+                defaultSettings={{
+                    centerCoordinate: CENTER,
+                    zoomLevel: 15
+                    }}
+                maxBounds={BOUNDS}
             />
             {children}
         </MapView>
