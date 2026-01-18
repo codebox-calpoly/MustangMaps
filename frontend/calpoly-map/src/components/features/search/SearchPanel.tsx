26f8af98b6c7ef5d3ff5182bf0f24fe907a1ed64
Nick E. <109779364+pbandjs1@users.noreply.github.com>
2026-01-17 17:05:43 -0800

    feat: add flatlist and formatting for searchable list

--- frontend/calpoly-map/src/components/features/search/SearchPanel.tsx
+++ frontend/calpoly-map/src/components/features/search/SearchPanel.tsx
@@ -1,22 +1,38 @@
-import React, { useCallback, useState } from "react";
-import { Pressable, StyleSheet, TextInput, View } from "react-native";
+import React, { useCallback, useMemo, useState } from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { useSearch } from "../../../hooks/useSearch";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import geoData from "./test.json";
 
 export function SearchPanel() {
-  const [ search, setSearch ] = useState("")
+  const [search, setSearch] = useState("");
+  const [count, setcount] = useState(0);
 
   const handleSearch = useCallback((input: string) => {
-    setSearch(input)
-  }, [])
+    setSearch(input);
+  }, []);
+
+  const data = geoData.features;
+
+  const filteredData = useMemo(() => {
+    const filteredData = data.filter((item) => item.properties.name);
+
+    return filteredData;
+  }, [geoData]);
 
   return (
-    <SafeAreaView
-    style={styles.searchContainer}>
+    <SafeAreaView style={styles.searchContainer}>
       <TextInput
-      style={styles.input}
+        style={styles.input}
         placeholder="Type Destination Here..."
         clearButtonMode="always"
         autoCapitalize="none"
@@ -24,13 +40,32 @@ export function SearchPanel() {
         onChangeText={handleSearch}
         value={search}
       />
+
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => {
+          return item.id
+        }}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            {/* insert logo based on type of building */}
+            <Text style={{fontSize: 50}}>{item.properties.building.charAt(0).toUpperCase()}</Text>
+            <View>
+              <Text style={styles.buildingName}>
+                {item.properties.name ? item.properties.name : "none"}
+              </Text>
+              <Text style={styles.subscript}>{item.id}</Text>
+            </View>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   searchContainer: {
-    marginHorizontal: 20,
+    marginHorizontal: 10
   },
   input: {
     paddingHorizontal: 20,
@@ -39,4 +74,27 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 8,
   },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  buildingName: {
+    fontSize: 17,
+    marginLeft: 10,
+    fontWeight: "600",
+  },
+  subscript: {
+    fontSize: 14,
+    marginLeft: 10,
+    color: "grey"
+  }
+
+  
 });
