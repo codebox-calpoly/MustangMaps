30707dfcdd69fabd94e1536bfc18b2efebad9ebc
Scout Knight-Pheng <sknightp@calpoly.edu>
2026-01-22 18:31:27 -0800

    fix: import stylesheet

--- frontend/calpoly-map/src/app/App.tsx
+++ frontend/calpoly-map/src/app/App.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import {StyleSheet} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { MapContainer } from '../components/map/MapContainer';
 import { BuildingLayer } from '../components/map/layers/BuildingLayer';
