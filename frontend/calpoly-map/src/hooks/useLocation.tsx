48bb3540e0eb6155d94c85d51276ff6d63e72af4
Scout Knight-Pheng <sknightp@calpoly.edu>
2026-01-27 23:00:13 -0800

    feat: add comments

--- frontend/calpoly-map/src/hooks/useLocation.tsx
+++ frontend/calpoly-map/src/hooks/useLocation.tsx
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
+// Retrieves user's latitude and longitude
 const UseLocation = () => {
   const [errorMsg, setErrorMsg] = useState<string>("");
   const [longitude] = useState<number | null>(null);
@@ -10,6 +11,7 @@ const UseLocation = () => {
   const getUserLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
 
+    // Returns error message if location not granted
     if (status !== "granted") {
       setErrorMsg("Permission to location was not granted");
       return;
