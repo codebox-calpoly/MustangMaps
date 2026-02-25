ebdd7dc68738b00f80c44e97bc5f5d0fd30bd1bc
Siddharth Balaji <siddharthbalaji6@gmail.com>
2026-02-25 09:03:54 -0800

    feat: rendered buildingpopup already implemented by daniel into app

--- frontend/calpoly-map/src/components/map/MapContainer.tsx
+++ frontend/calpoly-map/src/components/map/MapContainer.tsx
@@ -33,6 +33,7 @@ import type {
 } from "geojson";
 import { useMapContext } from "../../context/MapContext";
 import UserLocationMarker from "./markers/UserLocationMarker";
+import { BuildingPopup } from "./BuildingPopup";
 import { useUserLocation } from "../../context/UserLocationContext";
 import Animated, {
   useAnimatedStyle,
@@ -514,6 +515,12 @@ export function MapContainer({
         />
       )}
 
+      <BuildingPopup
+        visible={!!selectedBuilding}
+        building={selectedBuilding}
+        onClose={clearSelection}
+        onNavigate={handleNavigate}
+      />
     </View>
   );
 }
